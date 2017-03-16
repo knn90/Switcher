edb0f7453d7968725f835a693c4137685a194a79
Nguyen Khoi <nguyenkhoi2190@gmail.com>
2017-03-16 14:01:47 +0800

    Update to use auto layout. Can add switcher through xib or storyboard with custom image or color

--- KNSwitcher.swift
+++ KNSwitcher.swift
@@ -9,89 +9,91 @@
 import UIKit
 
 protocol KNSwitcherChangeValueDelegate {
-    func switcherDidChangeValue(KNSwitch:KNSwitcher,value:Bool)
+    func switcherDidChangeValue(switcher:KNSwitcher,value:Bool)
 }
-
-
 
 class KNSwitcher: UIView {
 
     var button: UIButton!
+    var buttonLeftConstraint: NSLayoutConstraint!
     var delegate: KNSwitcherChangeValueDelegate?
     
-    var on:Bool = false
-    var selectedColor:UIColor = UIColor(red: 126/255.0, green: 134/255.0, blue: 249/255.0, alpha: 1)
-    var originalColor:UIColor = UIColor(red: 243/255.0, green: 229/255.0, blue: 211/255.0, alpha: 1)
-    var originalImage:UIImage = UIImage(named: "Delete")!
-    var selectedImage:UIImage = UIImage(named: "Checkmark")!
+    @IBInspectable var on: Bool = false
     
-    private var offCenterPosition:CGPoint!
-    private var onCenterPosition:CGPoint!
+    @IBInspectable var originalImage:UIImage?
+    @IBInspectable var selectedImage:UIImage?
     
-     init(frame: CGRect,on: Bool) {
+    @IBInspectable var selectedColor:UIColor = UIColor(red: 126/255.0, green: 134/255.0, blue: 249/255.0, alpha: 1)
+    @IBInspectable var originalColor:UIColor = UIColor(red: 243/255.0, green: 229/255.0, blue: 211/255.0, alpha: 1)
+    
+    private var offCenterPosition: CGFloat!
+    private var onCenterPosition: CGFloat!
+    
+     init(frame: CGRect, on: Bool) {
         super.init(frame: frame)
         self.on = on
-        self.backgroundColor = UIColor.white
-        button = UIButton(type: .custom) // let preferred over var here
-        button.frame =  CGRect(x: 0, y:0 , width: frame.height * 0.75, height: frame.height * 0.75 )
-        button.center = CGPoint(x: button.frame.width/2 + 5, y: frame.height/2)
+        commonInit()
+    }
+    
+    override func awakeFromNib() {
+        commonInit()
+    }
+    
+    private func commonInit() {
+        button = UIButton(type: .custom)
+        self.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(switcherButtonTouch(_:)), for: UIControlEvents.touchUpInside)
-        
         button.setImage(originalImage, for: .normal)
         button.setImage(selectedImage, for: .selected)
+        offCenterPosition = self.bounds.height * 0.1
+        onCenterPosition = self.bounds.width - (self.bounds.height * 0.9)
         
-        onCenterPosition = CGPoint(x:self.frame.width - (button.frame.width/2) - 5  , y: self.frame.height/2)
-        offCenterPosition = CGPoint(x: button.frame.width/2 + 5 , y: self.frame.height/2)
-        animationSwitcherButton()
-        self.addSubview(button)
-    }
-    
-    func setImages(onImage:UIImage , offImage :UIImage)
-        {
-            button.setImage(offImage, for: .normal)
-            button.setImage(onImage, for: .selected)
-        }
-    func setOn(on:Bool)  {
-        if(on != self.on)
-        {
-            self.on = on
-            animationSwitcherButton()
-        }
-        else
-        {
-        self.on = on
-        }
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        self.layer.cornerRadius = self.frame.height / 2
-        self.clipsToBounds = true
-        button.layer.cornerRadius = button.frame.height / 2
- 
         if on == true {
             self.button.backgroundColor = selectedColor
         } else {
             self.button.backgroundColor = originalColor
         }
+        
+        if self.backgroundColor == nil {
+            self.backgroundColor = .white
+        }
+        initLayout()
+        animationSwitcherButton()
     }
     
-
-
-     func switcherButtonTouch(_ sender: AnyObject) {
-        on = !on;
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.bounds.height / 2
+        self.clipsToBounds = true
+        button.layer.cornerRadius = button.bounds.height / 2
+    }
+    
+    private func initLayout() {
+        button.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        buttonLeftConstraint = button.leftAnchor.constraint(equalTo: self.leftAnchor)
+        buttonLeftConstraint.isActive = true
+        button.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.8).isActive = true
+        button.widthAnchor.constraint(equalTo: button.heightAnchor, multiplier: 1).isActive = true
+    }
+    
+    func setImages(onImage:UIImage , offImage :UIImage) {
+            button.setImage(offImage, for: .normal)
+            button.setImage(onImage, for: .selected)
+        }   
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func switcherButtonTouch(_ sender: AnyObject) {
+        on = !on
         animationSwitcherButton()
-        delegate?.switcherDidChangeValue(KNSwitch: self, value: on)
+        delegate?.switcherDidChangeValue(switcher: self, value: on)
     }
     
     func animationSwitcherButton() {
         if on == true {
-            
             // Rotate animation
             let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
             rotateAnimation.fromValue = -CGFloat(M_PI)
@@ -101,20 +103,19 @@ class KNSwitcher: UIView {
             self.button.layer.add(rotateAnimation, forKey: "rotate")
             
             // Translation animation
-            UIView.animate(withDuration: 0.5, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: { () -> Void in
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: UIViewAnimationOptions.curveEaseInOut, animations: { () -> Void in
                 self.button.isSelected = true
-                self.button.center = self.onCenterPosition
+                self.buttonLeftConstraint.constant = self.onCenterPosition
                 self.layoutIfNeeded()
                 self.button.backgroundColor = self.selectedColor
                 }, completion: { (finish:Bool) -> Void in
                     self.button.layer.shadowOffset = CGSize(width: 0, height: 0.2)
                     self.button.layer.shadowOpacity = 0.3
-                    self.button.layer.shadowRadius = 5.0
+                    self.button.layer.shadowRadius = self.offCenterPosition
                     self.button.layer.cornerRadius = self.button.frame.height / 2
                     self.button.layer.shadowPath = UIBezierPath(roundedRect: self.button.layer.bounds, cornerRadius: self.button.frame.height / 2).cgPath
             })
         } else {
-            
             // Clear Shadow
             self.button.layer.shadowOffset = CGSize.zero
             self.button.layer.shadowOpacity = 0
@@ -130,15 +131,13 @@ class KNSwitcher: UIView {
             rotateAnimation.isCumulative = false;
             self.button.layer.add(rotateAnimation, forKey: "rotate")
             
-            UIView.animate(withDuration: 0.5, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: { () -> Void in
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: UIViewAnimationOptions.curveEaseInOut, animations: { () -> Void in
                 self.button.isSelected = false
-                self.button.center = self.offCenterPosition
+                self.buttonLeftConstraint.constant = self.offCenterPosition
                 self.layoutIfNeeded()
                 self.button.backgroundColor = self.originalColor
                 }, completion: { (finish:Bool) -> Void in
-                    
             })
-
         }
     }
 }
--- SwitcherExample/ViewController.swift
+++ SwitcherExample/ViewController.swift
@@ -9,28 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController, KNSwitcherChangeValueDelegate {
-    var big:KNSwitcher!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       // let view = KNSwitcher(frame: , on: true)
-         big = KNSwitcher(frame: CGRect(x: 20, y: 20, width: 250, height: 80),on: true)
-
-        big.setImages(onImage: UIImage(named: "happy")!, offImage: UIImage(named: "sad")!)
-        big.delegate = self;
-        self.view.addSubview(big)
-        
-        let view1 = KNSwitcher(frame: CGRect(x: 150, y: 150, width: 100, height: 20),on: false)
-        view1.selectedColor = UIColor.green
-        view1.originalColor = UIColor.orange
-        view1.selectedImage =  UIImage(named: "happy")!
-        view1.originalImage =  UIImage(named: "sad")!
-        view1.delegate = self;
-        self.view.addSubview(view1)
-        
-        let view2 = KNSwitcher(frame: CGRect(x: 50, y: 250, width: 85, height: 30),on: true)
-        view2.delegate = self;
-        self.view.addSubview(view2)
+        let switcher = KNSwitcher(frame: CGRect(x: 0, y: 0, width: 200, height: 40), on: false)
+        self.view.addSubview(switcher)
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,10 +23,10 @@ class ViewController: UIViewController, KNSwitcherChangeValueDelegate {
     }
     
     @IBAction func toggleBig(_ sender: AnyObject) {
-        big.setOn(on: !big.on)
+//        big.setOn(on: !big.on)
     }
  
-    func switcherDidChangeValue(KNSwitch:KNSwitcher, value: Bool) {
+    func switcherDidChangeValue(switcher:KNSwitcher, value: Bool) {
         if value {
             
         }
